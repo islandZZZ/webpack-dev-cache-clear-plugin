cb253e98358856e0157d14e9d568945851b9dce3
island <17629359576@163.com>
2022-06-01 11:22:11 +0800

    test: check test success

--- test/run.spec.js
+++ test/run.spec.js
@@ -3,7 +3,7 @@ const { runTask, domainErrMsg } = require("../dependency/run");
 describe('Test runTask', () => {
     test('Invalid domains length', () => {
         return runTask([]).catch(e => {
-            expect(e).toBe(1)
+            expect(e).toBe(domainErrMsg)
         })
     });
 });
